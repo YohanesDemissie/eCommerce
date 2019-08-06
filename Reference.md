623a7c151b9d2f2df89930fd80996d51f3090b37
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-05 23:29:53 -0700

    added fonts links to public html file and font-famnily to heaers and body text in app.js

--- Reference.md
+++ Reference.md
@@ -1,3 +1,5 @@
 #Source Code 
 Go to chapter 4 for zip file and yarn start to get a glimpse of finished professional product
 
+#USE YARN INSTEAD OF NPM FOR INSTALLING PACKAGES
+yarn add styled-components
--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        ITS A MIYAH MARIOOO!
-        LETSAH GOOHW!
-      </div>
+      <>
+      <h1>Welcome to Grizzy</h1>
+      <GlobalStyle />
+        <div>
+          ITS A MIYAH MARIOOO!
+          LETSAH GOOHW!
+        </div>
+      </>
     );
   }
 }
