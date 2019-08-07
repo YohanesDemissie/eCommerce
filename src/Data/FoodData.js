1fe984e85a35b395565ada1daf1eff1bd175a408
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-08-07 00:31:29 -0700

    all images rendering with simple elegant ui

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -8,6 +8,35 @@ export const foods = [
     img: './img/chefs-special.jpeg'
   },
   {
-
+    name: 'Classic Cheese',
+    img: './img/classic-cheese.jpeg'
+  },
+  {
+    name: 'Fiery Jalepeno',
+    img: './img/firey-jalepeno.jpeg'
+  },
+  {
+    name: 'Garden Special',
+    img: './img/garden-special.jpeg'
+  },
+  {
+    name: 'Garlic Ham',
+    img: './img/garlic-ham.jpeg'
+  },
+  {
+    name: 'Italian Mozzerela Special',
+    img: './img/italian-mozzy-special.jpeg'
+  },
+  {
+    name: 'Supreme Sausage',
+    img: './img/supreme-sausage.jpeg'
+  },
+  {
+    name: 'Zesty Italian',
+    img: './img/zesty-italian.jpeg'
+  },
+  {
+    name: 'Supreme Italian Combo',
+    img: './img/supreme-italian-combo.jpg'
   }
 ]
