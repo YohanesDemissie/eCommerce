6250220c328c6aacdc2ba33681566dd06e05bb98
Yohanes Demissie <Yohanesdemissie@gmail.com>
2024-11-06 15:45:18 -0800

    Updates

---
+++ precache-manifest.24f607df19638cd17dc5aa15b05bc3da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da6d25e441a49181e6ec",
+    "url": "/eCommerce/static/css/main.b68c5362.chunk.css"
+  },
+  {
+    "revision": "da6d25e441a49181e6ec",
+    "url": "/eCommerce/static/js/main.6dcf45e9.chunk.js"
+  },
+  {
+    "revision": "0be06942ba90b6a6bd4b",
+    "url": "/eCommerce/static/js/runtime~main.e1bb107d.js"
+  },
+  {
+    "revision": "75963fae63a99c712734",
+    "url": "/eCommerce/static/js/2.80b2b489.chunk.js"
+  },
+  {
+    "revision": "d64886824b1aa680ed91c8e61a48eff9",
+    "url": "/eCommerce/index.html"
+  }
+];
